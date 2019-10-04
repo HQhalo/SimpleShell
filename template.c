a32f14a709f74c5bee78ce4ba6f811779249e22c
HQuang <hquang@HQuangs-MBP.local>
2019-10-04 12:42:25 +0700

    Update template.c
    fix master

--- template.c
+++ template.c
@@ -24,7 +24,10 @@ void __ini__(){
 
 char *getInput(){
     getline(&bufferConst,&bufsize,stdin);
-    bufferConst[strlen(bufferConst)-1] = '\0'; // remoce enter character
+    bufferConst[strlen(bufferConst)-1] = '\0'; // remove enter character
+    if(strcmp(bufferConst,"!!")==0){
+        return history;
+    }
     memcpy(history,bufferConst,strlen(bufferConst));
     return bufferConst;
 }
@@ -33,28 +36,83 @@ char *getInput(){
 // return type of command: FLAG_PIPE or FLAG_NONE_PIPE 
 // matrix commands has 1 or 2 string,  
 int processLine(char *line,char **commands){
+    commands[0] = strtok(line,"|");
+    commands[1] = strtok(NULL,"|");
 
+    if(commands[1] != NULL){
+        return FLAG_PIPE;
+    }
+    else {
+        return FLAG_PIPE_NONE;
+    }
+    return -1;
 }
-
+int separateSpace(char *command,char **args){
+    int i =0;
+    char *token;
+    token = strtok(command," ");
+    while(token != NULL){
+        args[i] = token;
+        i++;
+        token = strtok(NULL," ");
+    }
+    
+    if(strcmp(args[i-1],"&")==0){
+       
+        args[i-1] = NULL;
+        return FLAG_WAIT;
+    }
+    else {
+        return FLAG_WAIT_NONE;
+    }
+    return -1;
+}
 //
 // return FLAG_REDIRECTION_L or FLAG_REDIRECTION_R or FLAG_REDIRECTION_NONE
 // separte command 
 // if command has redirection: redir contain output or input name file
-// flagWait = 1 if command end by &, = FLAG_HISTORY if command == "!!"
-int processCommand(char *command,char **args, char *redir,int *flagWait){
+// flagWait = 1 if command end by &
+int processCommand(char *command,char **args, char **redir,int *flagWait){
+        // printf("__%s__\n",command);
+    char *tempCommand;
+    char *tempDir;
+    if(strchr(command,'<')){
+        tempCommand = strtok(command,"<");
+        tempDir = strtok(NULL,"<");
+        
+        separateSpace(tempCommand,args);
+        *flagWait = separateSpace(tempDir,redir);
 
+        return FLAG_REDIRECTION_L;
+    }
+    else if(strchr(command,'>')){
+        tempCommand = strtok(command,">");
+        tempDir = strtok(NULL,">");
+        
+        separateSpace(tempCommand,args);
+        *flagWait = separateSpace(tempDir,redir);
+        return FLAG_REDIRECTION_R;
+    }
+    else {
+        //printf("__%s__\n",command);
+        *flagWait = separateSpace(command,args);
+        return FLAG_REDIRECTION_NONE;
+    }
+
+    *flagWait = -1;
+    return -1;
 }
 
-void execRedirR(char** args,int *flagWait){
+void execRedirR(char** args,int flagWait){
 
 }
-void execRedirL(char** args,int *flagWait){
+void execRedirL(char** args,int flagWait){
 
 }
 void execPipe(char **commands){
 
 }
-void execArgs(char** args,int *flagWait) 
+void execArgs(char** args,int flagWait) 
 { 
     pid_t pid = fork();  
     if (pid == -1) { 
@@ -66,7 +124,8 @@ void execArgs(char** args,int *flagWait)
         } 
         exit(0); 
     } else {
-        if(flagWait == 1){ 
+        if(flagWait == FLAG_WAIT){ 
+            printf("Waiting for finish child.\n");
             wait(NULL);  
         }
         return; 
@@ -76,7 +135,7 @@ void execArgs(char** args,int *flagWait)
 int main(){
     char * args[MAX_LINE/2+1];
     char *commands[2];
-    char redir[MAX_LINE];
+    char *redir[MAX_LINE/2+1];
     
     char *buffer = NULL;
     int shouldRun = 1;
@@ -90,13 +149,10 @@ int main(){
     while (shouldRun == 1){
         printf("\nosh>");  
         fflush(stdout);
-        if(flagHistory == FLAG_HISTORY){
-            buffer = history;
-            flagHistory = -1;
-        }
-        else {
-            buffer = getInput();
-        }
+       
+        
+        buffer = getInput();
+        
         flagPipe = processLine(buffer,commands);
 
         if(flagPipe == FLAG_PIPE){
@@ -106,12 +162,14 @@ int main(){
         else if(flagPipe == FLAG_PIPE_NONE){
             // Redirection
             int r = processCommand(commands[0],args,redir,&flagWait);
-
             if(r == FLAG_REDIRECTION_NONE){
                 //
+               
                 if(flagWait == FLAG_HISTORY){
+                    printf("his");
                     flagHistory = FLAG_HISTORY;
                     buffer = history;
+                    continue;
                 }
                 else {
                     execArgs(args,flagWait);
@@ -122,8 +180,9 @@ int main(){
                 execRedirR(args,flagWait);
             }else if(r == FLAG_REDIRECTION_L){
                 //
-                exceRedirL(args,flagWait);
+                execRedirL(args,flagWait);
             }
+            
 
         }
         
